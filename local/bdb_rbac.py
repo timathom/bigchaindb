ea27c0aca4dbf0e9153b744e8f294f7597e49823
timathom <timathom@gmail.com>
2018-10-22 16:26:42 -0400

    Update JSON-LD in test script.

--- local/bdb_rbac.py
+++ local/bdb_rbac.py
@@ -28,17 +28,17 @@ bf_item = {"data": {
 
 iul_bf_item_metadata = {
     "@context": {
-        "bf": "http://id.loc.gov/ontologies/bibframe/",
+        "bf": "bf:",
         "bflc": "http://id.loc.gov/ontologies/bflc/",
-        "rdfs": "http://www.w3.org/2000/01/rdf-schema#"
+        "rdfs": "rdfs:"
     },
     "@graph": [
         {
             "@id": "http://example.org/ocm05084045#Item050-8",
-            "@type": ["http://id.loc.gov/ontologies/bibframe/Item"],
-            "http://id.loc.gov/ontologies/bibframe/heldBy": [{"@id": "http://id.loc.gov/vocabulary/organizations/inuli"}],
-            "http://id.loc.gov/ontologies/bibframe/itemOf": [{"@id": "http://example.org/ocm05084045#Instance"}],
-            "http://id.loc.gov/ontologies/bibframe/note": [
+            "@type": ["bf:Item"],
+            "bf:heldBy": [{"@id": "http://id.loc.gov/vocabulary/organizations/inuli"}],
+            "bf:itemOf": [{"@id": "http://example.org/ocm05084045#Instance"}],
+            "bf:note": [
                 {"@id": "_:N981e348f02414549b1f45d3c4fd27c9f"},
                 {"@id": "_:Ne3fc3e8f6e114267a9c92869846eacaa"},
                 {"@id": "_:N5d07eeca480f4e49a76445d0b017b5cb"},
@@ -48,52 +48,52 @@ iul_bf_item_metadata = {
                 {"@id": "_:Nc7fe3b12110a4d24b842a787acbe3ba5"},
                 {"@id": "_:Nae49e8c1a2724cb3a1e32b9caabaab87"}
             ],
-            "http://id.loc.gov/ontologies/bibframe/shelfMark": [{"@id": "_:N86583b665c5a48bea6e69601f242ca82"}]
+            "bf:shelfMark": [{"@id": "_:N86583b665c5a48bea6e69601f242ca82"}]
         },
         {
             "@id": "_:N981e348f02414549b1f45d3c4fd27c9f",
-            "@type": ["http://id.loc.gov/ontologies/bibframe/Note"],
-            "http://www.w3.org/2000/01/rdf-schema#label": [{"@value": "First book printed from movable types in Europe; New Testament only from v. 2,in itself imperfect. GKW 4201 (\"not after Aug. 1456\"). BMC I, p. 17; Lilly Library Publication V, no. 1 (this copy, described in extenso); Goff B-526. Removed in 1953 from copy of v. 2 known as Trier II, in censuses as De Ricci 15 [b], Schwenke 14, Lazare 42, Norman 34."}]
+            "@type": ["bf:Note"],
+            "rdfs:label": [{"@value": "First book printed from movable types in Europe; New Testament only from v. 2,in itself imperfect. GKW 4201 (\"not after Aug. 1456\"). BMC I, p. 17; Lilly Library Publication V, no. 1 (this copy, described in extenso); Goff B-526. Removed in 1953 from copy of v. 2 known as Trier II, in censuses as De Ricci 15 [b], Schwenke 14, Lazare 42, Norman 34."}]
         },
         {
             "@id": "_:N86583b665c5a48bea6e69601f242ca82",
-            "@type": ["http://id.loc.gov/ontologies/bibframe/ShelfMarkLcc"],
-            "http://www.w3.org/2000/01/rdf-schema#label": [{"@value": "BS75 1454"}]
+            "@type": ["bf:ShelfMarkLcc"],
+            "rdfs:label": [{"@value": "BS75 1454"}]
         },
         {
             "@id": "_:Ne3fc3e8f6e114267a9c92869846eacaa",
-            "@type": ["http://id.loc.gov/ontologies/bibframe/Note"],
-            "http://www.w3.org/2000/01/rdf-schema#label": [{"@value": "Provenance: Trier--Wiernick--Rosenbach--Houghton--Scribner--Poole."}]
+            "@type": ["bf:Note"],
+            "rdfs:label": [{"@value": "Provenance: Trier--Wiernick--Rosenbach--Houghton--Scribner--Poole."}]
         },
         {
             "@id": "_:N5d07eeca480f4e49a76445d0b017b5cb",
-            "@type": ["http://id.loc.gov/ontologies/bibframe/Note"],
-            "http://www.w3.org/2000/01/rdf-schema#label": [{"@value": "Misbound. Collates (as GKW): [II--t9 ( -t10) v-xp10s yp10s ( -y6,9) zp10s Ap10s ( -A4) B-Cp10s Ep11s ( -E2,6) Dp12s ( -D1) F-Gp10s Hp5s ( -H1) Ip10s ( -I5-10)]. Missing leaves replaced with blanks."}]
+            "@type": ["bf:Note"],
+            "rdfs:label": [{"@value": "Misbound. Collates (as GKW): [II--t9 ( -t10) v-xp10s yp10s ( -y6,9) zp10s Ap10s ( -A4) B-Cp10s Ep11s ( -E2,6) Dp12s ( -D1) F-Gp10s Hp5s ( -H1) Ip10s ( -I5-10)]. Missing leaves replaced with blanks."}]
         },
         {
             "@id": "_:N0d5051ba76e24fcfab1392c5105f4981",
-            "@type": ["http://id.loc.gov/ontologies/bibframe/Note"],
-            "http://www.w3.org/2000/01/rdf-schema#label": [{"@value": "A few contemporary and later marginalia; ms. table of contents v. 2 dated 1569; later notes by Wyttenbach and others of Trier Stadtbibliothek."}]
+            "@type": ["bf:Note"],
+            "rdfs:label": [{"@value": "A few contemporary and later marginalia; ms. table of contents v. 2 dated 1569; later notes by Wyttenbach and others of Trier Stadtbibliothek."}]
         },
         {
             "@id": "_:N3047d53d3f1c4e77a5c432c5ae5118f2",
-            "@type": ["http://id.loc.gov/ontologies/bibframe/Note"],
-            "http://www.w3.org/2000/01/rdf-schema#label": [{"@value": "Assigned title; first leaf present begins: Beatissimo pape damaso ieronim(us) ..."}]
+            "@type": ["bf:Note"],
+            "rdfs:label": [{"@value": "Assigned title; first leaf present begins: Beatissimo pape damaso ieronim(us) ..."}]
         },
         {
             "@id": "_:N0194bf02bfc2457db18150714c9e9fd4",
-            "@type": ["http://id.loc.gov/ontologies/bibframe/Note"],
-            "http://www.w3.org/2000/01/rdf-schema#label": [{"@value": "Initials supplied in colors, incipits in red, chapter numbers red and/or black."}]
+            "@type": ["bf:Note"],
+            "rdfs:label": [{"@value": "Initials supplied in colors, incipits in red, chapter numbers red and/or black."}]
         },
         {
             "@id": "_:Nc7fe3b12110a4d24b842a787acbe3ba5",
-            "@type": ["http://id.loc.gov/ontologies/bibframe/Note"],
-            "http://www.w3.org/2000/01/rdf-schema#label": [{"@value": "Imperfect: lacks all before II:190; for numbers and text of missing N.T. leaves, cf. Lilly Library Publication V."}]
+            "@type": ["bf:Note"],
+            "rdfs:label": [{"@value": "Imperfect: lacks all before II:190; for numbers and text of missing N.T. leaves, cf. Lilly Library Publication V."}]
         },
         {
             "@id": "_:Nae49e8c1a2724cb3a1e32b9caabaab87",
-            "@type": ["http://id.loc.gov/ontologies/bibframe/Note"],
-            "http://www.w3.org/2000/01/rdf-schema#label": [{"@value": "Bound in 16th-century calf over wooden boards, tooled in blind, defective; rebacked."}]
+            "@type": ["bf:Note"],
+            "rdfs:label": [{"@value": "Bound in 16th-century calf over wooden boards, tooled in blind, defective; rebacked."}]
         }
     ]
 }
